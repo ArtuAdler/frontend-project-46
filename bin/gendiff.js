507789b67c6576da834722b48381498f9ab0c112
artu <carapina936@gmail.com>
2022-09-25 21:53:29 +0300

    step 4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-import genDiff from '../src/index.js';
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -11,9 +12,7 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2))
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
-
-
--- src/index.js
+++ src/index.js
@@ -5,50 +5,42 @@ import _ from 'lodash';
 const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const stringify = (data, symb = ' ', count = 3) => {
-    const tab = symb.repeat(count);
-    let x = JSON.stringify(data, null, tab)
-    return x.replaceAll('"', '').replaceAll(',', '')
-}
+  const tab = symb.repeat(count);
+  const x = JSON.stringify(data, null, tab);
+  return x.replaceAll('"', '').replaceAll(',', '');
+};
 
 const findDifference = (obj1, obj2) => {
-    const keys = _.union(_.keys(obj1), _.keys(obj2));
-    const newObj = {};
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-
-    for (const k of keys1) {
-        if (!keys2.includes(k)) {
-            newObj['- ' + [k]] = obj1[k];
-        }
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const keys = _.union(keys1, keys2);
+  const newObj = keys.reduce((acc, key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      acc[`+ ${[key]}`] = obj2[key];
+    } else if (!Object.hasOwn(obj2, key)) {
+      acc[`- ${[key]}`] = obj1[key];
+    } else if (keys1.includes(key) && keys2.includes(key)) {
+      if (obj2[key] !== obj1[key]) {
+        acc[`- ${[key]}`] = obj1[key];
+        acc[`+ ${[key]}`] = obj2[key];
+      } else {
+        acc[`  ${key}`] = obj1[key];
+      }
     }
-    for (const k of keys) {
-        if (keys2.includes(k) && keys1.includes(k)) {
-            if (obj2[k] !== obj1[k]) {
-                newObj['- ' + [k]] = obj1[k];
-                newObj['+ ' + [k]] = obj2[k];
-            }
-            if (obj2[k] === obj1[k]) {
-                newObj['  ' + k] = obj1[k];
-            }
-        }
-    }
-    for (const k of keys2) {
-        if (!keys1.includes(k)) {
-            newObj['+ ' + [k]] = obj2[k];
-        }
-    }
-    return newObj;
+    return acc;
+  }, {});
+  return newObj;
 };
 
 const genDiff = (file1, file2) => {
-    const data1 = readFileSync(getFilePath(file1), 'utf-8');
-    const data2 = readFileSync(getFilePath(file2), 'utf-8');
+  const data1 = readFileSync(getFilePath(file1), 'utf-8');
+  const data2 = readFileSync(getFilePath(file2), 'utf-8');
 
-    const obj1 = JSON.parse(data1);
-    const obj2 = JSON.parse(data2);
+  const obj1 = JSON.parse(data1);
+  const obj2 = JSON.parse(data2);
 
-    const answer = findDifference(obj1, obj2);
-    return stringify(answer);
-}
+  const answer = findDifference(obj1, obj2);
+  return stringify(answer);
+};
 
 export default genDiff;
