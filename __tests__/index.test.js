ee3c78c5dc5c9b3ad14bab266f974ed8965bc73d
artu <carapina936@gmail.com>
2022-09-28 23:10:00 +0300

    step4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,14 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const x = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const answer = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
 
 test('genDiff', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json').toEqual(x));
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(answer);
 });
-
-
-// console.log(x)
--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const findDifference = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
-  const newObj = keys.reduce((acc, key) => {
+  const sortedKeys = _.sortBy(keys);
+  const newObj = sortedKeys.reduce((acc, key) => {
     if (!Object.hasOwn(obj1, key)) {
       acc[`+ ${[key]}`] = obj2[key];
     } else if (!Object.hasOwn(obj2, key)) {
