1b6893f549d0a085a07d40aa3057296bac683787
artu <carapina936@gmail.com>
2022-11-03 21:34:05 +0300

    edits

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,30 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtures = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
 
-const answer = readFileSync(getFixturePath('stylish.txt'), 'utf-8');
+const expectedStylish = readFixtures('stylish.txt');
+const expectedPlain = readFixtures('plain.txt');
+const expectedJSON = readFixtures('json.txt');
 
-const answerPlain = readFileSync(getFixturePath('plain.txt'), 'utf-8');
-
-test('stylish', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(answer.trim());
-});
-
-test('yaml', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(answer.trim());
-});
-
-test('plain', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(answerPlain.trim());
+describe('genDiff', () => {
+  test('should be work with json', () => {
+    expect(genDiff('file1.json', 'file2.json')).toEqual(expectedStylish.trim());
+    expect(genDiff('file1.json', 'file2.json', 'stylish')).toEqual(expectedStylish.trim());
+    expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(expectedPlain.trim());
+    expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJSON.trim());
+  });
+  test('should be work with yaml', () => {
+    expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(expectedStylish.trim());
+    expect(genDiff('file1.yaml', 'file2.yaml', 'stylish')).toEqual(expectedStylish.trim());
+    expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(expectedPlain.trim());
+    expect(genDiff('file1.yaml', 'file2.yaml', 'json')).toEqual(expectedJSON.trim());
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,52 +1,23 @@
-import path, { dirname } from 'path';
+import path from 'path';
 import { readFileSync } from 'fs';
-import _ from 'lodash';
-import { fileURLToPath } from 'url';
 import parse from './parsers.js';
 import getDiff from './formatters/index.js';
+import compare from './compare.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 
-const compare = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  const keysUnion = _.union(keys1, keys2);
-  const sortedKeysUnion = _.sortBy(keysUnion);
-
-  return sortedKeysUnion.reduce((acc, key) => {
-    const object1Value = obj1[key];
-    const object2Value = obj2[key];
-    if (_.isPlainObject(object1Value) && _.isPlainObject(object2Value)) {
-      const object = { [key]: { type: 'children', value: compare(object1Value, object2Value) } };
-      return { ...acc, ...object };
-    }
-    if (object1Value === object2Value) {
-      return { ...acc, [key]: { type: 'unchanged', value: object1Value } };
-    }
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      return { ...acc, [key]: { type: 'changed', oldValue: object1Value, newValue: object2Value } };
-    }
-    if (_.has(obj1, key)) {
-      return { ...acc, [key]: { type: 'deleted', value: object1Value } };
-    }
-    if (_.has(obj2, key)) {
-      return { ...acc, [key]: { type: 'added', value: object2Value } };
-    }
-    return acc;
-  }, {});
-};
-
-const genDiff = (file1, file2, formatN) => {
+const genDiff = (file1, file2, format) => {
   const data1 = readFileSync(getFixturePath(file1), 'utf-8');
   const data2 = readFileSync(getFixturePath(file2), 'utf-8');
-  const format1 = getFixturePath(file1).split('.')[1];
-  const format2 = getFixturePath(file2).split('.')[1];
+
+  const format1 = path.extname(file1).slice(1);
+  const format2 = path.extname(file2).slice(1);
+
   const obj1 = parse(data1, format1);
   const obj2 = parse(data2, format2);
-  const answer = compare(obj1, obj2);
-  return getDiff(answer, formatN);
+
+  const tree = compare(obj1, obj2);
+  return getDiff(tree, format);
 };
+
 export default genDiff;
