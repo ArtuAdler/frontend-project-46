217004770e9280227a5e793557c5b3c0aae0ce2e
artu <carapina936@gmail.com>
2022-11-09 11:07:38 +0300

    edits

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,11 +8,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
-const readFixture = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
+const readFixture = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8').trim();
 
-const expectedStylish = readFixture('stylish.txt').trim();
-const expectedPlain = readFixture('plain.txt').trim();
-const expectedJSON = readFixture('json.txt').trim();
+const expectedStylish = readFixture('stylish.txt');
+const expectedPlain = readFixture('plain.txt');
+const expectedJSON = readFixture('json.txt');
 
 describe('genDiff', () => {
   test('should be work with json', () => {
@@ -24,13 +24,11 @@ describe('genDiff', () => {
     expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedJSON);
   });
   test('should be work with yaml', () => {
-    const filepath11 = getFixturePath('file1.yaml');
-    const filepath22 = getFixturePath('file2.yaml');
-    expect(genDiff(filepath11, filepath22)).toEqual(expectedStylish);
-    expect(genDiff(filepath11, filepath22, 'stylish')).toEqual(expectedStylish);
-    expect(genDiff(filepath11, filepath22, 'plain')).toEqual(expectedPlain);
-    expect(genDiff(filepath11, filepath22, 'json')).toEqual(expectedJSON);
+    const filepath1 = getFixturePath('file1.yaml');
+    const filepath2 = getFixturePath('file2.yaml');
+    expect(genDiff(filepath1, filepath2)).toEqual(expectedStylish);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedJSON);
   });
 });
-
-// expect(genDiff('file1.yaml', 'file2.yaml', 'json')).toEqual(expectedJSON);
